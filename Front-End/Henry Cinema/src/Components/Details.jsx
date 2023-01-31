bda74750a9000c6368ba57f9bf67f7442e79baf0
unknown <juanjc2002@hotmail.com>
2023-01-31 16:21:30 -0300

    added details Componen (not finished)

---
+++ Front-End/Henry Cinema/src/Components/Details.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+
+//trabajando (Juan Cruz)
+
+export default function Details() {
+  return (
+    <div>
+        <div>
+            <h1>Genre</h1>
+        </div>
+        <div>
+            <h2>Title</h2>
+        </div>
+        <div>
+        <p>Duration</p>
+        <p>esrb rating</p>
+        <p>Genre</p>
+
+        </div>
+        <button>Buy tickets</button>
+        //trailer
+        //imagen de la portada 
+        <div>
+            <p>all details</p>
+        </div>
+        <div>
+            <img src="" alt="image"/>
+        </div>
+
+    </div>
+  )
+}
