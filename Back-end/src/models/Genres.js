9581b93ea49eb6a487e69444929f669012bc96f2
Your Name <you@example.com>
2023-01-31 19:35:27 -0300

    cree modelos peliculas y generos

---
+++ Back-end/src/models/Genres.js
@@ -0,0 +1,18 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+  sequelize.define('genre', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    }
+  },
+  {
+    timestamps: false
+  });
+};
