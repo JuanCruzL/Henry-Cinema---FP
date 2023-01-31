3e2bea0eb0feb4a99bcd2d2d4291dc2e04a31cc9
Deybi1004 <deybiorrego@gmail.com>
2023-01-30 21:45:12 -0500

    solucionado

--- Back-end/bugFinish.js
+++ Back-end/bugFinish.js
@@ -1,6 +1,2 @@
-console.log("bug arreglado 2");
-console.log("bug arreglado 3");
-console.log("bug arreglado 4");
-console.log("bug arreglado 5");
-console.log("bug arreglado ");
-
+let saludar = "hola"
+console.log(saludar);
