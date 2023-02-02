84426ac5259e448717ddecc7b653bcf8f6849e92
ZTPspartan <alfonsos99710@gmail.com>
2023-02-02 18:26:48 -0500

    HOME/CSS/ dandole funcionalidad para recibir datos 1/3

---
+++ Front-End/Henry-Cinema/src/Components/Home/Data.js
@@ -0,0 +1,42 @@
+export const cartelera = [
+    {
+        img: "https://images.hdqwalls.com/wallpapers/warhammer-40000-hc.jpg"
+        , id: 1
+    },
+    {
+        img: "https://preview.redd.it/ugxqxhphbxh51.jpg?width=5118&format=pjpg&auto=webp&s=74f65624d4b5a5267a69a134c0ba3b7c1a147cf5"
+        , id: 2
+    },
+    {
+        img: "https://wallpapers.com/images/featured/e1dpbba47ubbh6o0.jpg"
+        , id: 3
+    },
+    {
+        img: "https://coolwallpapers.me/picsup/1386527-warhammer-40k.jpg"
+        , id: 4
+    },
+    {
+        img: "https://wallpapers.com/images/hd/tempest-guards-warhammer-40k-hd-4gi03hnxyzkqlrza.jpg"
+        , id: 5
+    },
+    {
+        img: "https://images.hdqwalls.com/wallpapers/warhammer-40000-hc.jpg"
+        , id: 6
+    },
+    {
+        img: "https://preview.redd.it/ugxqxhphbxh51.jpg?width=5118&format=pjpg&auto=webp&s=74f65624d4b5a5267a69a134c0ba3b7c1a147cf5"
+        , id: 7
+    },
+    {
+        img: "https://coolwallpapers.me/picsup/1386527-warhammer-40k.jpg"
+        , id: 8
+    },
+    {
+        img: "https://wallpapers.com/images/featured/e1dpbba47ubbh6o0.jpg"
+        , id: 9
+    },
+    {
+        img: "https://wallpapers.com/images/hd/tempest-guards-warhammer-40k-hd-4gi03hnxyzkqlrza.jpg"
+        , id: 10
+    },
+];
