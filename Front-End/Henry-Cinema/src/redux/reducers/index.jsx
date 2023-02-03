297817b4a5db93ebf70f6becc90cf865ac0a9f1e
paula <acostapaula7223@gmail.com>
2023-02-03 17:27:58 -0500

    action

--- Front-End/Henry-Cinema/src/redux/reducers/index.jsx
+++ Front-End/Henry-Cinema/src/redux/reducers/index.jsx
@@ -1,3 +1,5 @@
+import { GET_MOVIES } from "../actions";
+
 const initialState = {
   movieId: [],
   movies: [],
@@ -6,34 +8,30 @@ const initialState = {
 
 const rootReducer = (state = initialState, action) => {
   switch (action.type) {
-    case "GET_MOVIES": {
+    case GET_MOVIES: {
       return {
         ...state,
         movies: action.payload,
-        allMovies: action.payload
+        allMovies: action.payload,
       };
     }
     case "AGE_CLASSIFICATION":
-      const allM= state.allMovies;
-      const age_classification =allM.filter(data =>data.classification===payload)
-     return{
+      const allM = state.allMovies;
+      const age_classification = allM.filter(
+        (data) => data.classification === payload
+      );
+      return {
         ...state,
-        movies: age_classification
-     }
-     case "GET_MOVIE_ID":
-       return {
-         ...state,
-         movieId: action.payload
-       }
+        movies: age_classification,
+      };
+    case "GET_MOVIE_ID":
+      return {
+        ...state,
+        movieId: action.payload,
+      };
     default:
       return state;
   }
 };
 
 export default rootReducer;
-
-
-
-
-
-
