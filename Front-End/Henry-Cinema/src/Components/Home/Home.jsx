b3d60f3f87e24f5327c99016aeefef207cb3292f
ZTPspartan <alfonsos99710@gmail.com>
2023-02-03 14:06:41 -0500

    HOME/CSS pequeño ajuste

--- Front-End/Henry-Cinema/src/Components/Home/Home.jsx
+++ Front-End/Henry-Cinema/src/Components/Home/Home.jsx
@@ -14,14 +14,14 @@ export default function Home() {
     const dispatch = useDispatch()
 
 
-    const [currentPage,setCurrentPage]=useState(1);//* Creamos una constante ponde guardar/setear la pagina actual(1) 
-    const [moviesPerPage, setMoviesPerPage]=useState(8);//* Creamos una constante para escoger el limite de peliculas por pagina(8)
-    const indexOfLastMovie=currentPage * moviesPerPage;//* Obtenemos el indice del ultimo elemento de la pagina actual
-    const indexOfFirstMovie=indexOfLastMovie - moviesPerPage;//* Obtenemos el indice del primer elemento de la pagina actual
-    const currentMovie=peliculas.slice(indexOfFirstMovie,indexOfLastMovie);//* Obtenemos los datos entre los 2 indices anteriores
+    const [currentPage, setCurrentPage] = useState(1);//* Creamos una constante ponde guardar/setear la pagina actual(1) 
+    const [moviesPerPage, setMoviesPerPage] = useState(8);//* Creamos una constante para escoger el limite de peliculas por pagina(8)
+    const indexOfLastMovie = currentPage * moviesPerPage;//* Obtenemos el indice del ultimo elemento de la pagina actual
+    const indexOfFirstMovie = indexOfLastMovie - moviesPerPage;//* Obtenemos el indice del primer elemento de la pagina actual
+    const currentMovie = peliculas.slice(indexOfFirstMovie, indexOfLastMovie);//* Obtenemos los datos entre los 2 indices anteriores
 
 
-    const paginated=(pageNumber)=>{
+    const paginated = (pageNumber) => {
         setCurrentPage(pageNumber)
     }
 
@@ -48,65 +48,66 @@ export default function Home() {
 
     return (
         <div id="Switch" className="light-mode">
-            <div className="navHome">
-                <div className="grid-container">
-                    <a>Henry CINEMA</a>
-                    <a>CINEMAS</a>
-                    <a>MOVIES</a>
-                    <a>FOOD & DRINKS</a>
-                    <a>About</a>
-                    <div className="SearchBar">
-                        <input type="text" placeholder="SearchMovie" className="pagi" />
-                        <button type="submit" className="pagiBo">Search</button>
+            <div className="Homehome">
+                <div className="navHome">
+                    <div className="grid-container">
+                        <a>Henry CINEMA</a>
+                        <a>CINEMAS</a>
+                        <a>MOVIES</a>
+                        <a>FOOD & DRINKS</a>
+                        <a>About</a>
+                        <div className="SearchBar">
+                            <input type="text" placeholder="SearchMovie" className="pagi" />
+                            <button type="submit" className="pagiBo">Search</button>
+                        </div>
+                        <button type="button" className="Switch" onClick={e => toggleDarkLight(e)} title="Toggle dark/light mode">🌑</button>
+                        <a>
+                            <img src="https://cdn.pixabay.com/photo/2020/07/14/13/07/icon-5404125_1280.png" width={30} height={30}></img>
+                        </a>
                     </div>
-                    <button type="button" className="Switch" onClick={e => toggleDarkLight(e)} title="Toggle dark/light mode">🌑</button>
-                    <a>
-                        <img src="https://cdn.pixabay.com/photo/2020/07/14/13/07/icon-5404125_1280.png" width={30} height={30}></img>
-                    </a>
-                </div>
-                <HomeCarrusel
-                    Prev={Prev}
-                    Next={Next}
-                    cartelera={cartelera}
-                />
-                <HomePaginated
-                peliculas={peliculas.length}
-                moviesPerPage={moviesPerPage}
-                paginated={paginated}
-                /> 
-                <HomeMovie
-                    peliculas={currentMovie}
-                />
-                <br></br>
-                <footer>
-                    <h1>CONTACT US</h1>
+                    <HomeCarrusel
+                        Prev={Prev}
+                        Next={Next}
+                        cartelera={cartelera}
+                    />
+                    <HomePaginated
+                        peliculas={peliculas.length}
+                        moviesPerPage={moviesPerPage}
+                        paginated={paginated}
+                    />
+                    <HomeMovie
+                        peliculas={currentMovie}
+                    />
                     <br></br>
-                    <div className="Contact">
-                        <div>
-                            <p>Contacto 1</p>
+                    <footer>
+                        <h1>CONTACT US</h1>
+                        <br></br>
+                        <div className="Contact">
+                            <div>
+                                <p>Contacto 1</p>
+                            </div>
+                            <div>
+                                <p>Contacto 2</p>
+                            </div>
+                            <div>
+                                <p>Contacto 3</p>
+                            </div>
+                            <div>
+                                <p>Contacto 4</p>
+                            </div>
+                            <div>
+                                <p>Contacto 5</p>
+                            </div>
+                            <div>
+                                <p>Contacto 6</p>
+                            </div>
+                            <div>
+                                <p>Contacto 7</p>
+                            </div>
                         </div>
-                        <div>
-                            <p>Contacto 2</p>
-                        </div>
-                        <div>
-                            <p>Contacto 3</p>
-                        </div>
-                        <div>
-                            <p>Contacto 4</p>
-                        </div>
-                        <div>
-                            <p>Contacto 5</p>
-                        </div>
-                        <div>
-                            <p>Contacto 6</p>
-                        </div>
-                        <div>
-                            <p>Contacto 7</p>
-                        </div>
-                    </div>
-                </footer>
+                    </footer>
+                </div>
             </div>
-
         </div>
     )
 }
