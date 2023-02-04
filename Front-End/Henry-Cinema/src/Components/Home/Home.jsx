c3a8b0414fddf67f85459ebc20a01a156b480bbc
ZTPspartan <alfonsos99710@gmail.com>
2023-02-03 21:11:09 -0500

    NAV/CSS/HOME añadidos al nav y paleta de color global

--- Front-End/Henry-Cinema/src/Components/Home/Home.jsx
+++ Front-End/Henry-Cinema/src/Components/Home/Home.jsx
@@ -10,50 +10,49 @@ import HomeMovie from "./HomeMovie/HomeMovie";
 import HomePaginated from "./HomePaginated/HomePaginated";
 
 export default function Home() {
-  const dispatch = useDispatch();
+    const dispatch = useDispatch();
 
-  const [currentPage, setCurrentPage] = useState(1); //* Creamos una constante ponde guardar/setear la pagina actual(1)
-  const [moviesPerPage, setMoviesPerPage] = useState(8); //* Creamos una constante para escoger el limite de peliculas por pagina(8)
-  const indexOfLastMovie = currentPage * moviesPerPage; //* Obtenemos el indice del ultimo elemento de la pagina actual
-  const indexOfFirstMovie = indexOfLastMovie - moviesPerPage; //* Obtenemos el indice del primer elemento de la pagina actual
-  const currentMovie = peliculas.slice(indexOfFirstMovie, indexOfLastMovie); //* Obtenemos los datos entre los 2 indices anteriores
+    const [currentPage, setCurrentPage] = useState(1); //* Creamos una constante ponde guardar/setear la pagina actual(1)
+    const [moviesPerPage, setMoviesPerPage] = useState(8); //* Creamos una constante para escoger el limite de peliculas por pagina(8)
+    const indexOfLastMovie = currentPage * moviesPerPage; //* Obtenemos el indice del ultimo elemento de la pagina actual
+    const indexOfFirstMovie = indexOfLastMovie - moviesPerPage; //* Obtenemos el indice del primer elemento de la pagina actual
+    const currentMovie = peliculas.slice(indexOfFirstMovie, indexOfLastMovie); //* Obtenemos los datos entre los 2 indices anteriores
 
-  const paginated = (pageNumber) => {
-    setCurrentPage(pageNumber);
-  };
+    const paginated = (pageNumber) => {
+        setCurrentPage(pageNumber);
+    };
 
-  useEffect(() => {
-    Prev();
-    Next();
-  }, []);
+    useEffect(() => {
+        Prev();
+        Next();
+    }, []);
 
-  function Prev() {
-    var fila = document.querySelector(".contenedorCarrusel");
-    var flechaIz = document.getElementById("Prev");
-    flechaIz.addEventListener("click", () => {
-      fila.scrollLeft -= fila.offsetWidth;
-    });
-  }
+    function Prev() {
+        var fila = document.querySelector(".contenedorCarrusel");
+        var flechaIz = document.getElementById("Prev");
+        flechaIz.addEventListener("click", () => {
+            fila.scrollLeft -= fila.offsetWidth;
+        });
+    }
 
-  function Next() {
-    var fila = document.querySelector(".contenedorCarrusel");
-    var flechaDe = document.getElementById("Next");
-    flechaDe.addEventListener("click", () => {
-      fila.scrollLeft += fila.offsetWidth;
-    });
-  }
+    function Next() {
+        var fila = document.querySelector(".contenedorCarrusel");
+        var flechaDe = document.getElementById("Next");
+        flechaDe.addEventListener("click", () => {
+            fila.scrollLeft += fila.offsetWidth;
+        });
+    }
 
-  return (
-    <div id="Switch" className="light-mode">
+    return (
+
         <div className="Homehome">
-            <Nav/>
+            <Nav />
             <div className="BodyHome">
-                <HomeCarrusel   Prev={Prev} Next={Next} cartelera={cartelera}/>
-                <HomePaginated  peliculas={peliculas.length}  moviesPerPage={moviesPerPage} paginated={paginated}/>
-                <HomeMovie  peliculas={currentMovie}/>
+                <HomeCarrusel Prev={Prev} Next={Next} cartelera={cartelera} />
+                <HomePaginated peliculas={peliculas.length} moviesPerPage={moviesPerPage} paginated={paginated} />
+                <HomeMovie peliculas={currentMovie} />
                 <br></br>
             </div>
         </div>
-    </div>
-)
+    )
 }
