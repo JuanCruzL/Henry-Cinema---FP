bef5708b41dc3266a62f0bc83d335961070d10c0
JuanCruzL <109559830+JuanCruzL@users.noreply.github.com>
2023-02-06 17:43:15 -0300

    Add files via upload
    restored files

---
+++ api/src/routes/moviesRoutes.js
@@ -0,0 +1,95 @@
+const { Router } = require("express");
+const { Movie } = require("../db");
+const { getMovies, getMovieById } = require("../controllers/movies");
+const router = Router();
+
+router.get("/", async (req, res) => {
+  const data = await getMovies();
+  const { name } = req.query;
+  if (name) {
+    const movieName = data.filter((m) =>
+      m.title.toLowerCase().includes(name.toLowerCase())
+    );
+    movieName.length
+      ? res.status(200).send(movieName)
+      : res.status(400).json({ error: "Movie does not exist" });
+  } else {
+    res.status(200).send(data);
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    let movieFound = await getMovieById(id);
+    res.status(200).send(movieFound);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+
+router.post("/", async (req, res) => {
+  let {
+    title,
+    imageVertical,
+    imageHorizontal,
+    voteAverage,
+    overview,
+    status,
+    productionCompanies,
+    review,
+    runtime,
+    origin,
+    genres,
+    directors,
+    actors,
+    video,
+    classification,
+    distributor,
+  } = req.body;
+
+  try {
+
+    await Movie.create({
+      title,
+      imageVertical,
+      imageHorizontal,
+      voteAverage,
+      overview,
+      status,
+      review,
+      productionCompanies,
+      runtime,
+      origin,
+      genres,
+      directors,
+      actors,
+      video,
+      classification,
+      distributor,
+    });
+    
+    res.status(200).send('MOVIE CREATED');
+  } catch (error) {
+    console.log(error);
+    res.status(400).send(error);
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    let { id } = req.params;
+    await Movie.destroy({
+      where: {
+        id,
+      },
+    });
+    res.sendStatus(204);
+    console.log("MOVIE ELIMINATED");
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+module.exports = router;
