7cd9de93efe2ac21243f004e9785ec5419492ffb
unknown <juanjc2002@hotmail.com>
2023-02-06 17:40:32 -0300

    files restored

--- Front-End/Henry-Cinema/src/Components/Login/login.jsx
+++ Front-End/Henry-Cinema/src/Components/Login/login.jsx
@@ -2,19 +2,68 @@ import React, { useState } from "react";
 import "./Login.css";
 import logo from "../Utils/logo-henry-cinema.png";
 
-
 export default function Login() {
+  const validate = (form) => {};
+
+  const [formUp, setFormUp] = useState({
+    fullName: "",
+    email: "",
+    password: "",
+    notifications: false,
+  });
+
+  const [formIn, setFormIn] = useState({
+    email: "",
+    password: "",
+  });
 
   const [sign, setSign] = useState("sign-in");
-  const [checked2, setChecked] = useState(false)
+  const [checked2, setChecked] = useState(false);
+
+  //----------------------------------------HANDLERS------------------------------------------
 
   const handleSign = (signstring) => {
     setSign(signstring);
   };
 
   const handleCheck = (e) => {
-    setChecked(e.target.checked)
+    setChecked(e.target.checked);
+  };
+
+  const handleSubmitIn = (e) => {
+    console.log(formIn)
+    e.preventDefault();
+    setFormIn({
+      email: "",
+      password: "",
+    })
+  };
+
+  const handleSubmitUp=(e) => {
+    e.preventDefault()
+    setFormUp({
+      fullName:"",
+      email: "",
+      password: "",
+      notifications: false,
+    })
   }
+
+  const handleChangeIn = (e) => {
+    setFormIn({
+      ...formIn,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleChangeUp = (e) => {
+    setFormUp({
+      ...formUp,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  //-------------------------------------------SIGN IN FORM-----------------------------------
 
   if (sign === "sign-in") {
     return (
@@ -34,19 +83,38 @@ export default function Login() {
                   Sign up
                 </p>
               </div>
-              <p className="desc-p">PASSWORD</p>
-              <input
-                placeholder="Enter your password"
-                type="password"
-                className="formInput"
-              />
-              <p className="desc-p">E-MAIL</p>
-              <input placeholder="Enter your Email" className="formInput" />
+              <form onSubmit={(e) => handleSubmitIn(e)} className="">
+                <p className="desc-p">E-MAIL</p>
+                <input
+                  placeholder="Enter your Email"
+                  className="formInput"
+                  name="email"
+                  value={formIn.email}
+                  onChange={(e) => handleChangeIn(e)}
+                />
+                <p className="desc-p">PASSWORD</p>
+                <input
+                  placeholder="Enter your password"
+                  type="password"
+                  className="formInput"
+                  name="password"
+                  value={formIn.password}
+                  onChange={(e) => handleChangeIn(e)}
+                />
+                {/* <button className="buttonSubmit">Sign in</button> */}
+              </form>
+              <p>{formIn.email}</p>
+              <p>{formIn.password}</p>
             </div>
           </div>
         </div>
       </div>
     );
+
+
+
+//--------------------------------------SIGN UP FORM-------------------------------------------
+
   } else if (sign === "sign-up") {
     return (
       <div className="loginContainer">
@@ -65,21 +133,46 @@ export default function Login() {
                   Sign up
                 </p>
               </div>
-              <p className="desc-p">FULL NAME</p>
-              <input placeholder="Enter your full name" className="formInput" />
-              <p className="desc-p">PASSWORD</p>
-              <input
-                placeholder="Enter your password"
-                type="password"
-                className="formInput"
-              />
-              <p className="desc-p">E-MAIL</p>
-              <input placeholder="Enter your Email" className="formInput" />
-              <label className="container">
-                <input  type="checkbox" className="checkinput" onClick={(e)=> handleCheck(e)}/>
-                <span className="checkmark"></span>
-                <div className="textdiv">I want to recieve e-mail notifications</div>
-              </label>
+              <form>
+                <p className="desc-p">FULL NAME</p>
+                <input
+                  placeholder="Enter your full name"
+                  className="formInput"
+                  onChange={e => handleChangeUp(e)}
+                  value={formUp.fullName}
+                  name="fullName"
+                />
+                <p className="desc-p">PASSWORD</p>
+                <input
+                  placeholder="Enter your password"
+                  type="password"
+                  className="formInput"
+                  value={formUp.password}
+                  name="password"
+                  onChange={e => handleChangeUp(e)}
+                />
+                <p className="desc-p">E-MAIL</p>
+                <input
+                  placeholder="Enter your Email"
+                  className="formInput"
+                  value={formUp.email}
+                  name="email"
+                  onChange={e => handleChangeUp(e)}
+                />
+                <label className="container">
+                  <input
+                    type="checkbox"
+                    className="checkinput"
+                    onClick={(e) => handleCheck(e)}
+                    value={formUp.notifications}
+                    name="notifications"
+                  />
+                  <span className="checkmark"></span>
+                  <div className="textdiv">
+                    I want to recieve e-mail notifications
+                  </div>
+                </label>
+              </form>
             </div>
           </div>
         </div>
