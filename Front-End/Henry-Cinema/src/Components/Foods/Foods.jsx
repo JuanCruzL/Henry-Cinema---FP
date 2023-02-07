80701062306b65154458d6821bef39f341306523
Deybi1004 <deybiorrego@gmail.com>
2023-02-06 22:54:43 -0500

    Componente Foods and Drinks

--- Front-End/Henry-Cinema/src/Components/Foods/Foods.jsx
+++ Front-End/Henry-Cinema/src/Components/Foods/Foods.jsx
@@ -1,26 +1,85 @@
-import React from "react";
+import React, { useState } from "react";
 import Nav from "../Nav/Nav";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 import { getFoods, getDrinks, getCombos } from "../../redux/actions/index";
+import Footer from "../footer/footer"
+
+import CardsFoods from "../Foods/CardsFoods/CardsFoods";
+import "./Foods.css";
 
 function Foods() {
   const dispatch = useDispatch();
 
   const allFoods = useSelector((state) => state.foods);
   const allDrinks = useSelector((state) => state.drinks);
-  console.log(allFoods);
+  const combos = useSelector((state) => state.combos);
+  const [seeFood, setSeeFoods] = useState("");
+
+  /* console.log(allFoods);
   console.log(allDrinks);
+  console.log(combos); */
 
   useEffect(() => {
     dispatch(getFoods());
     dispatch(getDrinks());
+    dispatch(getCombos());
   }, [dispatch]);
 
   return (
-    <div>
+    <div className="containerComponent">
       <Nav></Nav>
+      <div className="container-foods">
+        <div className="buttons-foods">
+          <div className="button-combos" onClick={() => setSeeFoods("combos")}>Combos</div>
+          <div className="button-foods" onClick={() => setSeeFoods("foods")}>Pop-Corn and Food</div>
+          <div className="button-drinks" onClick={() => setSeeFoods("drinks")}>Drinks</div>
+        </div>
+
+        {seeFood === "combos" && (<div className="containerForCards">
+          {seeFood === "combos" &&
+            combos.map((e) => (
+              <CardsFoods
+                key={e.id}
+                kind={seeFood}
+                name={e.name}
+                description={e.description}
+                price={e.price}
+                image={e.image}
+              ></CardsFoods>
+            ))}
+        </div>)}
+
+        {seeFood === "drinks" && (<div className="containerForCards">
+          {seeFood === "drinks" &&
+            allDrinks.map((e) => (
+              <CardsFoods
+                key={e.id}
+                kind={seeFood}
+                name={e.name}
+                description={e.description}
+                price={e.price}
+                image={e.image}
+              ></CardsFoods>
+            ))}
+        </div>)}
+
+        {seeFood === "foods" && (<div className="containerForCards">
+          {seeFood === "foods" &&
+            allFoods.map((e) => (
+              <CardsFoods
+                key={e.id}
+                kind={seeFood}
+                name={e.name}
+                description={e.description}
+                price={e.price}
+                image={e.image}
+              ></CardsFoods>
+            ))}
+        </div>)}
+      </div>
+        <Footer/>
     </div>
   );
 }
