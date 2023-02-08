435c5a2834fc936fd29e02c2cdd05a472e2f4d5f
ZTPspartan <alfonsos99710@gmail.com>
2023-02-07 19:52:25 -0500

    HOME/CSS super mejoramiento del carrusel home

--- Front-End/Henry-Cinema/src/Components/Home/HomeCarrusel/HomeCarrusel.jsx
+++ Front-End/Henry-Cinema/src/Components/Home/HomeCarrusel/HomeCarrusel.jsx
@@ -1,9 +1,107 @@
-import React from "react";
+import { useEffect } from "react";
 import "../HomeCarrusel/HomeCarrusel.css";
 
-export default function HomeCarrusel({ Prev, Next, cartelera }) {
+export default function HomeCarrusel({ cartelera }) {
+
+  useEffect(() => {
+    playpause();
+    mostrar(0)
+  },[cartelera])
+
+  var actual = 0;
+  const velocidad = 5000;
+  var tiempo = null;
+
+  var cantidad = document.getElementsByClassName("carteleraHome").length;
+
+  function puntos(n) {
+    var ptn = document.getElementsByClassName("punto");
+    for (let i = 0; i < ptn.length; i++) {
+      if (ptn[i].className.includes("activo")) {
+        ptn[i].className = ptn[i].className.replace("activo", "");
+        break;
+      }
+    }
+    ptn[n].className = "punto activo";
+  }
+  function mostrar(n) {
+    actual = n;
+    if (cantidad > 0) {
+      var imagenes = document.getElementsByClassName("carteleraHome");
+      for (let i = 0; i < imagenes.length; i++) {
+        if (imagenes[i].className.includes("actual")) {
+          imagenes[i].className = imagenes[i].className.replace("actual", "");
+          break;
+        }
+      }
+      imagenes[n].className = "carteleraHome actual";
+      puntos(n);
+    }
+  }
+
+  function Next() {
+    actual++;
+    if (actual >= cantidad) {actual = 0;}
+    mostrar(actual);
+  }
+  function Prev() {
+    actual--;
+    if (actual < 0) {actual = cantidad - 1;}
+    mostrar(actual);
+  }
+
+  function playpause() {
+    if (document.getElementById("btnPauseHome")) {
+      var boton = document.getElementById("btnPauseHome");
+      if (tiempo == null) {
+        boton.src = "https://www.pngmart.com/files/3/Pause-Button-Transparent-Background.png";
+        tiempo = setInterval(() => { Next(); }, velocidad);
+      } else {
+        clearInterval(tiempo);
+        tiempo = null;
+        boton.src = "https://assets.stickpng.com/images/580b57fcd9996e24bc43c4f9.png";
+      }
+    }
+  }
   return (
     <div className="HomeCarrusel">
+      <div className="contenedorCarrusel">
+        <div className="Carrusel">
+          {cartelera.map((data) => {
+            return (
+              <div key={data.apiId} className="carteleraHome">
+                <img src={data.imageHorizontal}></img>
+              </div>
+            );
+          })}
+
+          <button role="button" id="Prev" className="Prev" onClick={() => Prev()}>{" "}&#10094;{" "}</button>
+
+          <button role="button" id="Next" className="Next" onClick={() => Next()}>{" "}&#10095;{" "}</button>
+
+          <div className="puntos">
+            {cartelera.map((data, index) => {
+              return (
+                index != 0 ?
+                  <span className="punto" onClick={() => mostrar(index)}></span>
+                  :<span className="punto activo" onClick={() => mostrar(index)}></span>
+              )
+            })
+            }
+
+          </div>
+
+          <div className="botonPause">
+            <img src="https://www.pngmart.com/files/3/Pause-Button-Transparent-Background.png" id="btnPauseHome" onClick={() => playpause()} />
+          </div>
+
+        </div>
+      </div>
+    </div>
+  );
+}
+
+{/* <div className="HomeCarrusel">
       <a>
         <button role="button"id="Prev"className="Prev"onClick={() => Prev()}>
           {" "}&#10094;{" "}
@@ -29,6 +127,4 @@ export default function HomeCarrusel({ Prev, Next, cartelera }) {
       <button role="button" id="Next" className="Next" onClick={() => Next()}>
         {" "}&#10095;{" "}
       </button>
-    </div>
-  );
-}
+    </div> */}
