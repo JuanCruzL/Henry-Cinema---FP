1c98728ba5cb180fdf3e3eeb555f9c4a5bc8ef98
unknown <juanjc2002@hotmail.com>
2023-02-07 21:39:53 -0300

    working in login

--- Front-End/Henry-Cinema/src/Components/Login/login.jsx
+++ Front-End/Henry-Cinema/src/Components/Login/login.jsx
@@ -3,7 +3,16 @@ import "./Login.css";
 import logo from "../Utils/logo-henry-cinema.png";
 
 export default function Login() {
-  const validate = (form) => {};
+  
+  const [sign, setSign] = useState("sign-in");
+  const [checked2, setChecked2] = useState(false);
+  const [errors, setErrors] = useState({
+    forminPassword: false,
+    forminEmail: false,
+    formupFullname: false,
+    formupPassword: false,
+    formupEmail: false,
+  });
 
   const [formUp, setFormUp] = useState({
     fullName: "",
@@ -17,18 +26,38 @@ export default function Login() {
     password: "",
   });
 
-  const [sign, setSign] = useState("sign-in");
-  const [checked2, setChecked] = useState(false);
 
-  //----------------------------------------HANDLERS------------------------------------------
+//-------------------------------------------HANDLERS-----------------------------------------------------
+
+
+//--------------------------------------SIGN IN VALIDATOR-------------------------------------------
+  (/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g).test(formIn.email) ? errors.forminEmail = false : errors.forminEmail = true;
+
+ /([\da-zA-Z]){8,}/.test(formIn.password) ? errors.forminPassword = false : errors.forminPassword = true;
+
+ const validate = (formUp) => {
+  if(errors.formupFullname === false && errors.formupEmail === false && errors.formupPassword === false) {
+    return true
+  }
+ }
+
+ //----------------------------------------SIGN UP VALIDATOR--------------------------------------
+ (/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g).test(formUp.email) ? errors.formupEmail = false : errors.formupEmail = true;
+    
+ /([\da-zA-Z]){8,}/.test(formUp.password) ? errors.formupPassword = false : errors.formupPassword = true;
+
+ /^(?!.* $)+[a-zA-Z][A-Za-z ]+$/.test(formUp.fullName) ? errors.formupFullname = false : errors.formupFullname = true
+
 
   const handleSign = (signstring) => {
     setSign(signstring);
   };
 
   const handleCheck = (e) => {
-    setChecked(e.target.checked);
+    setChecked2(e.target.checked)
+    formUp.notifications = e.target.checked
   };
+
 
   const handleSubmitIn = (e) => {
     console.log(formIn)
@@ -41,11 +70,12 @@ export default function Login() {
 
   const handleSubmitUp=(e) => {
     e.preventDefault()
+    alert("notificaciones en " + formUp.notifications)
     setFormUp({
       fullName:"",
       email: "",
       password: "",
-      notifications: false,
+      notifications: checked2,
     })
   }
 
@@ -54,6 +84,7 @@ export default function Login() {
       ...formIn,
       [e.target.name]: e.target.value,
     });
+    
   };
 
   const handleChangeUp = (e) => {
@@ -61,6 +92,7 @@ export default function Login() {
       ...formUp,
       [e.target.name]: e.target.value,
     });
+
   };
 
   //-------------------------------------------SIGN IN FORM-----------------------------------
@@ -75,7 +107,7 @@ export default function Login() {
           <div className="prueba">
             <div className="form-container">
               <div className="sign">
-                <p className="sign-inP" onClick={() => handleSign("sign-in")}>
+                <p className="sign-inPselect" onClick={() => handleSign("sign-in")}>
                   Sign in
                 </p>
                 <p className="signP">or</p>
@@ -83,28 +115,26 @@ export default function Login() {
                   Sign up
                 </p>
               </div>
-              <form onSubmit={(e) => handleSubmitIn(e)} className="">
-                <p className="desc-p">E-MAIL</p>
+              <form onSubmit={(e) => handleSubmitIn(e)} className="formsignin">
+                <p className={errors.forminEmail ? "baddesc-p": "desc-p"}>E-MAIL</p>
                 <input
                   placeholder="Enter your Email"
-                  className="formInput"
+                  className={errors.forminEmail ? "badinput" : "formInput"}
                   name="email"
                   value={formIn.email}
                   onChange={(e) => handleChangeIn(e)}
                 />
-                <p className="desc-p">PASSWORD</p>
+                <p className={errors.forminPassword ? "baddesc-p": "desc-p"}>PASSWORD</p>
                 <input
                   placeholder="Enter your password"
                   type="password"
-                  className="formInput"
+                  className={errors.forminPassword ? "badinput" : "formInput"}
                   name="password"
                   value={formIn.password}
                   onChange={(e) => handleChangeIn(e)}
                 />
-                {/* <button className="buttonSubmit">Sign in</button> */}
+                <button className="buttonSubmit" type="submit">Sign in</button>
               </form>
-              <p>{formIn.email}</p>
-              <p>{formIn.password}</p>
             </div>
           </div>
         </div>
@@ -129,42 +159,44 @@ export default function Login() {
                   Sign in
                 </p>
                 <p className="signP">or</p>
-                <p className="sign-upP" onClick={() => handleSign("sign-up")}>
+                <p className="sign-upPselect" onClick={() => handleSign("sign-up")}>
                   Sign up
                 </p>
+                <p>{String(formUp.notifications)}</p>
               </div>
-              <form>
-                <p className="desc-p">FULL NAME</p>
+              <form onSubmit={(e) => handleSubmitUp(e)}>
+                <p className={errors.formupFullname ? "baddesc-p": "desc-p"}>FULL NAME</p>
                 <input
                   placeholder="Enter your full name"
-                  className="formInput"
+                  className={errors.formupFullname ? "badinput" : "formInput"}
                   onChange={e => handleChangeUp(e)}
                   value={formUp.fullName}
                   name="fullName"
                 />
-                <p className="desc-p">PASSWORD</p>
+                <p className={errors.formupPassword ? "baddesc-p": "desc-p"}>PASSWORD</p>
                 <input
                   placeholder="Enter your password"
                   type="password"
-                  className="formInput"
+                  className={errors.formupPassword ? "badinput" : "formInput"}
                   value={formUp.password}
                   name="password"
                   onChange={e => handleChangeUp(e)}
                 />
-                <p className="desc-p">E-MAIL</p>
+                <p className={errors.formupEmail ? "baddesc-p" : "desc-p"}>E-MAIL</p>
                 <input
                   placeholder="Enter your Email"
-                  className="formInput"
+                  className={errors.formupEmail ? "badinput" : "formInput"}
                   value={formUp.email}
                   name="email"
                   onChange={e => handleChangeUp(e)}
                 />
+                <button className="buttonSubmitup" type="submit">Sign Up</button>
+              </form>
                 <label className="container">
                   <input
                     type="checkbox"
                     className="checkinput"
                     onClick={(e) => handleCheck(e)}
-                    value={formUp.notifications}
                     name="notifications"
                   />
                   <span className="checkmark"></span>
@@ -172,7 +204,6 @@ export default function Login() {
                     I want to recieve e-mail notifications
                   </div>
                 </label>
-              </form>
             </div>
           </div>
         </div>
@@ -180,3 +211,4 @@ export default function Login() {
     );
   }
 }
+
