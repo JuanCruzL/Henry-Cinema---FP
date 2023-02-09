1ccfd83a1201331c5c8bbcde03d3f03901b4b40b
Samuel <samuel.esba@hotmail.com>
2023-02-08 21:05:37 -0400

    testing

--- api/src/routes/loginRoutes.js
+++ api/src/routes/loginRoutes.js
@@ -3,7 +3,7 @@ const { Router } = require('express');
 const { verifyLogin }  = require("../controllers/login");
 const router = Router();
 
-router.get("/", async (req, res) => {
+router.post("/", async (req, res) => {
     const loginForm = req.body;
     try {
         res.status(200).send(await verifyLogin(loginForm));
