f75efb98d5049f8f863ca3059bd065fe84228886
unknown <juanjc2002@hotmail.com>
2023-02-10 19:09:12 -0300

    fixed conflicts

--- Front-End/Henry-Cinema/src/redux/actions.jsx
+++ Front-End/Henry-Cinema/src/redux/actions.jsx
@@ -11,6 +11,8 @@ import {
   GET_COMBOS,
   REQUEST_GENRES,
   SEARCH_FOOD,
+  GET_CURRENT_USER,
+  LOGIN_OR_REGISTER_USER_WITH_GOOGLE,
 } from "./actionTypes";
 
 //MOVIES
@@ -146,6 +148,9 @@ export const requestGenders = () => {
   };
 };
 
+
+
+
 // crea el usuario y lo guarda en la base de datos
 export const signUp = (payload) => {
   return async (dispatch) => {
@@ -157,6 +162,15 @@ export const signUp = (payload) => {
       console.log(e)
     }
   }
+}
+
+
+
+//.............. 
+export function iconNav(){
+  return {
+    type: "MODO",
+  };
 }
 
 
@@ -175,50 +189,65 @@ export const signUp = (payload) => {
 export const logInUserWithGoogle = (response) => {
   return async (dispatch) => {
     try {
-      const { email, givenName } = response.profileObj;
+      const { email, given_name } = response;
+      console.log(email, given_name);
       const userCreated = await axios.post(
         `http://localhost:3001/login/google`,
-        { email, userName: givenName }
+        { email, userName: given_name }
       );
-      console.log(userCreated.data);
       return dispatch({
-        type: "POST_USER_WITH_GOOGLE",
+        type: LOGIN_OR_REGISTER_USER_WITH_GOOGLE,
         payload: userCreated.data,
       });
+
     } catch (error) {
       console.log("el error de logInUserWithGoogle es:", error.message);
     }
-  };
-};
+  }
+}
+
+
+
+
+
+
+
+
+
 
 // actions.js
+
+
+
+
+
+
 
 // busca en la base de datos al usuario y lo logea con su token faltaria navigates en el componente
 
 export const logInUser = (email, password) => {
   if (!email && !password) {
-    return message.warn("Completa los campos para ingresar");
+    return console.log("Completa los campos para ingresar");
   }
   if (!email) {
-    return message.warn("Ingresa correo electronico");
+    return console.log("Ingresa correo electronico");
   }
 
   if (!password) {
-    return message.warn("Ingresa tu contraseña");
+    return console.log("Ingresa tu contraseña");
   }
 
   try {
     return async (dispatch) => {
-      const loginCredentials = await axios.post("http://localhost:3001/login", {
-        email,
-        password,
-      });
-      console.log(loginCredentials.data);
+      const loginCredentials = await axios.post("http://localhost:3001/login",
+        {email, password},
+      );
+      // console.log(loginCredentials.data);
       return dispatch({
-        type: "GET_CURRENT_USER",
+        type: GET_CURRENT_USER,
         payload: loginCredentials.data,
       });
-    };
+    }  
   } catch (error) {
     console.log(error);
   }
