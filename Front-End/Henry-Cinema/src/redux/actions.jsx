534516b9be17da86f272c8b6799d17f061403358
emagarc <wemagw@gmail.com>
2023-02-11 14:19:16 -0300

    Dashboard

--- Front-End/Henry-Cinema/src/redux/actions.jsx
+++ Front-End/Henry-Cinema/src/redux/actions.jsx
@@ -1,26 +1,28 @@
 import axios from "axios";
+const AGE_CLASSIFICATION = "AGE_CLASSIFICATION";
+export const GET_MOVIES = "GET_MOVIES";
+export const GET_RELEASES = "GET_RELEASES";
+export const REQUEST_GENDERS = "REQUEST_GENDERS";
+export const GET_SEATS = "GET_SEATS";
 import {
-  GET_MOVIES,
   GET_MOVIE_ID,
-  GET_RELEASES,
   SEARCH_MOVIE,
   DELETE_MOVIE,
-  AGE_CLASSIFICATION,
   GET_FOODS,
   GET_DRINKS,
   GET_COMBOS,
   REQUEST_GENRES,
   SEARCH_FOOD,
-  GET_CURRENT_USER,
-  LOGIN_OR_REGISTER_USER_WITH_GOOGLE,
 } from "./actionTypes";
 
+//axios.defaults.baseURL = "http://localhost:3001"
+axios.defaults.baseURL = "https://henry-cinema-fp-production.up.railway.app/";
 //MOVIES
 
 export const getMovies = () => {
   return (dispatch) => {
     axios
-      .get(`http://localhost:3001/movies`)
+      .get(`/movies`)
       .then((response) => {
         dispatch({
           type: GET_MOVIES,
@@ -36,7 +38,7 @@ export const getMovies = () => {
 export const getMovieById = (id) => {
   try {
     return async (dispatch) => {
-      let movieInfo = await axios.get(`http://localhost:3001/movies/${id}`);
+      let movieInfo = await axios.get(`/movies/${id}`);
       return dispatch({
         type: GET_MOVIE_ID,
         payload: movieInfo.data,
@@ -50,7 +52,7 @@ export const getMovieById = (id) => {
 export const getRelease = (id) => {
   try {
     return async (dispatch) => {
-      let movieInfo = await axios.get(`http://localhost:3001/nextReleases`);
+      let movieInfo = await axios.get(`/nextReleases`);
       return dispatch({
         type: GET_RELEASES,
         payload: movieInfo.data,
@@ -76,10 +78,10 @@ export function createMovie(movie) {
 export const deleteMovie = (id) => {
   return async function (dispatch) {
     try {
-      const response = await axios.delete(`http://localhost:3001/movies/${id}`);
+      const response = await axios.delete(`/movies/${id}`);
       if (response.data === "The movie has been removed") {
-        //const allMovies = await axios.get(`http://localhost:3001/movies`);
-        return dispatch({ type: DELETE_MOVIE });
+        const allMovies = await axios.get(`/movies`);
+        return dispatch({ type: DELETE_MOVIE, payload: allMovies.data });
       }
     } catch (error) {
       console.log(error.message);
@@ -106,7 +108,7 @@ export const searchMovie = (payload) => {
 export const getFoods = () => {
   try {
     return async (dispatch) => {
-      let allFoodsData = await axios.get("http://localhost:3001/foods");
+      let allFoodsData = await axios.get("/foods");
       return dispatch({
         type: GET_FOODS,
         payload: allFoodsData.data,
@@ -120,7 +122,7 @@ export const getFoods = () => {
 export const getDrinks = () => {
   try {
     return async (dispatch) => {
-      let allDrinksData = await axios.get("http://localhost:3001/drinks");
+      let allDrinksData = await axios.get("/drinks");
       return dispatch({
         type: GET_DRINKS,
         payload: allDrinksData.data,
@@ -134,7 +136,7 @@ export const getDrinks = () => {
 export const getCombos = () => {
   try {
     return async (dispatch) => {
-      let allCombosData = await axios.get("http://localhost:3001/combos");
+      let allCombosData = await axios.get("/combos");
       return dispatch({
         type: GET_COMBOS,
         payload: allCombosData.data,
@@ -152,6 +154,25 @@ export const searchFood = (payload) => {
   };
 };
 
+// actions.js
+export const getasientos = () => {
+  return (dispatch) => {
+    axios
+      .get(`/seats`)
+      .then((response) => {
+        dispatch({
+          type: GET_SEATS,
+          payload: response.data,
+        });
+      })
+      .catch((error) => {
+        console.log("error");
+      });
+  };
+};
+
+// actions.js
+
 //GENRES
 
 export const requestGenders = () => {
@@ -165,10 +186,7 @@ export const signUp = (payload) => {
   return async (dispatch) => {
     try {
       console.log(payload);
-      const userCreated = await axios.post(
-        "http://localhost:3001/users",
-        payload
-      );
+      const userCreated = await axios.post("/users", payload);
       console.log(userCreated);
     } catch (e) {
       console.log(e);
@@ -176,27 +194,20 @@ export const signUp = (payload) => {
   };
 };
 
-//..............
-export function iconNav() {
-  return {
-    type: "MODO",
-  };
-}
-
 // action.js
 
 // busca o crear al usuario en la base de datos con sus datos de google
 export const logInUserWithGoogle = (response) => {
   return async (dispatch) => {
     try {
-      const { email, given_name } = response;
-      console.log(email, given_name);
-      const userCreated = await axios.post(
-        `http://localhost:3001/login/google`,
-        { email, userName: given_name }
-      );
+      const { email, givenName } = response.profileObj;
+      const userCreated = await axios.post(`/login/google`, {
+        email,
+        userName: givenName,
+      });
+      console.log(userCreated.data);
       return dispatch({
-        type: LOGIN_OR_REGISTER_USER_WITH_GOOGLE,
+        type: "POST_USER_WITH_GOOGLE",
         payload: userCreated.data,
       });
     } catch (error) {
@@ -211,25 +222,22 @@ export const logInUserWithGoogle = (response) => {
 
 export const logInUser = (email, password) => {
   if (!email && !password) {
-    return console.log("Completa los campos para ingresar");
+    return message.warn("Completa los campos para ingresar");
   }
   if (!email) {
-    return console.log("Ingresa correo electronico");
+    return message.warn("Ingresa correo electronico");
   }
 
   if (!password) {
-    return console.log("Ingresa tu contraseña");
+    return message.warn("Ingresa tu contraseña");
   }
 
   try {
     return async (dispatch) => {
-      const loginCredentials = await axios.post("http://localhost:3001/login", {
-        email,
-        password,
-      });
-      // console.log(loginCredentials.data);
+      const loginCredentials = await axios.post("/login", { email, password });
+      console.log(loginCredentials.data);
       return dispatch({
-        type: GET_CURRENT_USER,
+        type: "GET_CURRENT_USER",
         payload: loginCredentials.data,
       });
     };
