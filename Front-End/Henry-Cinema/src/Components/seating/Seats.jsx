458ffa634b699b6d34698f876eb59fef5cef7678
Your Name <you@example.com>
2023-02-11 06:11:33 -0300

    agregue mejoras al componente seating y cree el componente ticket

--- Front-End/Henry-Cinema/src/Components/seating/Seats.jsx
+++ Front-End/Henry-Cinema/src/Components/seating/Seats.jsx
@@ -1,56 +1,7 @@
 import React from "react";
 import Chair from "../../img/asiento.svg/"
 import './seating.css'
-// const Seats = ({ handleClick, seatsData, letra }) => {
-//   return (
-//     <div className="seats">
-//       <div className="Filaizquierda">   
-//         {seatsData
-//           .filter(seat => seat.row === letra && seat.number <= 20)
-//           .map(seat => (
-//             <button
-//               key={`${seat.row}${seat.number}`}
-//               className="asiento"
-//               data-value={`${seat.row}${seat.number}`}
-//               onClick={handleClick}
-//               disabled={seat.reserved}
-//               style={{ backgroundColor: seat.reserved ? "red" : "#40ff00" }}
-//             >
-//               <img
-//                 key={`${seat.row}${seat.number}`}
-//                 data-value={`${seat.row}${seat.number}`}
-//                 src={Chair}
-//                 alt="Asiento"
-//               />
-//             </button>
-//           ))}
-//       </div>
-//       <div className='Filaderecha'>  
-//         {seatsData
-//           .filter(seat => seat.row === letra && seat.number > 20 && seat.number <= 40)
-//           .map(seat => (
-//             <button
-//               key={`${seat.row}${seat.number}`}
-//               className="asiento"
-//               data-value={`${seat.row}${seat.number}`}
-//               onClick={handleClick}
-//               disabled={seat.reserved}
-//               style={{ backgroundColor: seat.reserved ? "red" : "#40ff00" }}
-//             >
-//               <img
-//                 key={`${seat.row}${seat.number}`}
-//                 data-value={`${seat.row}${seat.number}`}
-//                 src={Chair}
-//                 alt="Asiento"
-//               />
-//             </button>
-//           ))}
-//       </div>
-//     </div>
-//   );
-// };
 
-// export default Seats;
 const Seats = ({ handleClick, seatsData, letra, asientosSeleccionados }) => {
   return (
     <div className="seats">
@@ -63,8 +14,8 @@ const Seats = ({ handleClick, seatsData, letra, asientosSeleccionados }) => {
               className="asiento"
               data-value={`${seat.row}${seat.number}`}
               onClick={handleClick}
-              disabled={seat.reserved}
-              style={{ backgroundColor: asientosSeleccionados.includes(`${seat.row}${seat.number}`) ? "yellow" : seat.reserved ? "red" : "#40ff00" }}
+              disabled={seat.reserved || seat.payed}
+              style={{ backgroundColor: asientosSeleccionados.includes(`${seat.row}${seat.number}`) ? "yellow" : seat.reserved || seat.payed ? "red" : "#40ff00" }}
             >
               <img
                 key={`${seat.row}${seat.number}`}
@@ -84,8 +35,8 @@ const Seats = ({ handleClick, seatsData, letra, asientosSeleccionados }) => {
               className="asiento"
               data-value={`${seat.row}${seat.number}`}
               onClick={handleClick}
-              disabled={seat.reserved}
-              style={{ backgroundColor: asientosSeleccionados.includes(`${seat.row}${seat.number}`) ? "yellow" : seat.reserved ? "red" : "#40ff00" }}
+              disabled={seat.reserved || seat.payed}
+              style={{ backgroundColor: asientosSeleccionados.includes(`${seat.row}${seat.number}`) ? "yellow" : seat.reserved || seat.payed ? "red" : "#40ff00" }}
             >
               <img
                 key={`${seat.row}${seat.number}`}
