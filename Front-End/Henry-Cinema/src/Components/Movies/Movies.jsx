33303a8eae968755743b9ef518a6ce52794becd6
emagarc <wemagw@gmail.com>
2023-02-13 17:38:56 -0300

    filters fixed

--- Front-End/Henry-Cinema/src/Components/Movies/Movies.jsx
+++ Front-End/Henry-Cinema/src/Components/Movies/Movies.jsx
@@ -23,6 +23,15 @@ const Movies = () => {
   const [classifications, setClassifications] = useState([]);
   const [selectedClassification, setSelectedClassification] = useState(null);
   const [availableMovies, setAvailableMovies] = useState([]);
+  const [filteredState, setFilteredState] = useState(null);
+
+  //cosas para el nuevo filtro(probando)
+
+  const [filter, setFilter] = useState({
+    Genre: "",
+    Classification: "todos",
+    Calification: "Calification"
+  })
 
   const getClassifications = () => {
     const classificationsArray = Array.from(
@@ -74,60 +83,54 @@ const Movies = () => {
 
   //------------------filtro de generos-------------------------------
 
+  // const handleSelect = (event) => {
+  //   if (event.target.value === null) {
+  //     setSelectedGenre(null);
+  //     setFiltered(availableMovies);
+  //     return;
+  //   }
+  //   setSelectedGenre(event.target.value);
+  //   let filteredMovies = [];
+  //   if (Filtered.length > 0) {
+  //     filteredMovies = Filtered.filter((movie) =>
+  //       movie.genres.includes(event.target.value)
+  //     );
+  //   }
+  //   if (filteredMovies.length === 0) {
+  //     filteredMovies = allMovies.filter((movie) =>
+  //       movie.genres.includes(event.target.value)
+  //     );
+  //   }
+  //   setFiltered(filteredMovies);
+  //   setFilteredState(filteredMovies)
+  // };
+  // console.log(selectedGenre);
+
   const handleSelect = (event) => {
     if (event.target.value === null) {
       setSelectedGenre(null);
       setFiltered(availableMovies);
+      setFilteredState(null);
       return;
     }
     setSelectedGenre(event.target.value);
-    let filteredMovies = [];
-    if (Filtered.length > 0) {
-      filteredMovies = Filtered.filter((movie) =>
-        movie.genres.includes(event.target.value)
-      );
-    }
-    if (filteredMovies.length === 0) {
-      filteredMovies = allMovies.filter((movie) =>
-        movie.genres.includes(event.target.value)
-      );
-    }
+    let filteredMovies = allMovies.filter((movie) =>
+      movie.genres.includes(event.target.value)
+    );
     setFiltered(filteredMovies);
+    setFilteredState(filteredMovies);
   };
-  console.log(selectedGenre);
 
   //------------------- funcion que ordena por popularidad-------------------------------
 
-  // const handleCalificationSort = (event) => {
-  //   if (event.target.value === "Calification") {
-  //     return;
-  //   }
-  //   let sortedMovies = [...availableMovies];
-  //   if (Filtered.length === 0) {
-  //     sortedMovies.sort((a, b) => {
-  //       if (event.target.value === "More Popular") {
-  //         return b.voteAverage - a.voteAverage;
-  //       }
-  //       return a.voteAverage - b.voteAverage;
-  //     });
-  //   } else {
-  //     sortedMovies = [...Filtered].sort((a, b) => {
-  //       if (event.target.value === "More Popular") {
-  //         return b.voteAverage - a.voteAverage;
-  //       }
-  //       return a.voteAverage - b.voteAverage;
-  //     });
-  //   }
-  //   setAvailableMovies(sortedMovies);
-  //   setFiltered(sortedMovies);
-  // };
+
   const handleCalificationSort = (event) => {
     if (event.target.value === "Calification") {
       // setFiltered(availableMovies);
       return;
     }
     let sortedMovies;
-    if (!selectedGenre) {
+    if (!selectedGenre && !classifications) {
       sortedMovies = [...availableMovies];
     } else {
       sortedMovies = [...Filtered];
@@ -142,31 +145,126 @@ const Movies = () => {
     setFiltered(sortedMovies);
   };
 
-  console.log(classifications);
-  console.log(Filtered);
+  // console.log(classifications);
+  // console.log(Filtered);
 
   //--------------------filtro de clasification--------------------------------
+  // const handleClassificationFilter = (event) => {
+  //   if (event.target.value === "todos") {
+  //     setSelectedClassification(null);
+  //     setFiltered(availableMovies);
+  //     return;
+  //   }
+  //   setSelectedClassification(event.target.value);
+  //   let filteredMovies = [];
+  //   if (Filtered.length > 0) {
+  //     filteredMovies = Filtered.filter((movie) =>
+  //       movie.classification === event.target.value
+  //     );
+  //   }
+  //   if (filteredMovies.length === 0) {
+  //     filteredMovies = availableMovies.filter((movie) =>
+  //       movie.classification === event.target.value
+  //     );
+  //   }
+  //   setFiltered(filteredMovies);
+  // };
+
+
+
+
+
+  // const handleClassificationFilter = (event) => {
+  //   if (event.target.value === "todos") {
+  //     setSelectedClassification(null);
+  //     setFiltered(availableMovies);
+  //     setFilteredState(null);
+  //     setFilteredState(null);
+  //     setSelectedGenre();
+  //     return;
+  //   }
+
+
+  //   setSelectedClassification(event.target.value);
+  //   let filteredMovies = [];
+  //   if (filteredState) {
+  //     filteredMovies = filteredState.filter((movie) =>
+  //       movie.classification === event.target.value
+  //     );
+  //   } else {
+  //     filteredMovies = Filtered.filter((movie) =>
+  //       movie.classification === event.target.value
+  //     );
+  //   }
+  //   setFiltered(filteredMovies);
+  //   setFilteredState(Filtered);
+  // };
+
+
   const handleClassificationFilter = (event) => {
     if (event.target.value === "todos") {
       setSelectedClassification(null);
       setFiltered(availableMovies);
+      setFilteredState(null);
+      setSelectedGenre(null);
       return;
     }
+
     setSelectedClassification(event.target.value);
-    let filteredMovies = [];
-    if (Filtered.length > 0) {
-      filteredMovies = Filtered.filter((movie) =>
-        movie.classification === event.target.value
-      );
-    }
-    if (filteredMovies.length === 0) {
-      filteredMovies = allMovies.filter((movie) =>
-        movie.classification === event.target.value
-      );
-    }
+    let filteredMovies = filteredState.filter((movie) =>
+      movie.classification === event.target.value
+    );
     setFiltered(filteredMovies);
   };
-  
+
+
+
+  function FiltradosContenedor(e, filtroCambiado) {
+    let filteredMovies = allMovies;
+    let actual = e.target.value;
+
+    /* FILTRO DE GENERO */
+    if (filtroCambiado == "Genre") {
+      actual != "" ? filteredMovies = filteredMovies.filter((movie) => movie.genres.includes(actual)) : filteredMovies;
+      setFilter({ ...filter, Genre: actual })
+    } else {
+      filter.Genre != "" ? filteredMovies = filteredMovies.filter((movie) => movie.genres.includes(filter.Genre)) : filteredMovies;
+    }
+
+    /* FILTRO DE CALIFICACION */
+    if (filtroCambiado == "Classification") {
+      actual!="todos"? filteredMovies = filteredMovies.filter((movie) =>movie.classification === actual):filteredMovies;
+      setFilter({...filter,classification:actual})
+    }else{
+      filter.Classification!="todos"? filteredMovies = filteredMovies.filter((movie) =>movie.classification === filter.Classification):filteredMovies;
+    }
+    
+    /* FILTRO DE MAS POLULARES */
+    if (filtroCambiado == "Calification") {
+      actual != "Calification" ?
+        filteredMovies.sort((a, b) => {
+          if (actual == "More Popular") {return b.voteAverage - a.voteAverage;}
+          return a.voteAverage - b.voteAverage;
+        })
+        : filteredMovies;
+
+      setFilter({ ...filter, Calification: actual });
+    }else{
+      filter.Calification != "Calification" ?
+        filteredMovies.sort((a, b) => {
+          if (filter.Calification == "More Popular") {return b.voteAverage - a.voteAverage;}
+          return a.voteAverage - b.voteAverage;
+        })
+        : filteredMovies;
+    }
+    /*======================================= */
+
+
+    // console.log(actual, " - ", filtroCambiado)
+    // console.log(filter)
+    // console.log(filteredMovies)
+    setFiltered(filteredMovies)
+  }
 
   return (
     <div className="Container">
@@ -180,9 +278,44 @@ const Movies = () => {
         </section>
 
         <section className="movies-filter">
+          <select onChange={(e) => FiltradosContenedor(e, "Genre")} >
+            <option value="" style={{ color: "red" }} >
+              Select a Genre
+            </option>
+            {genresCurrent.map((genre) => (
+              <option key={genre} value={genre} >
+                {genre}
+              </option>
+            ))}
+          </select>
+
+          <select onChange={(e) => FiltradosContenedor(e, "Classification")}>
+            <option value="todos" onClick={() => setSelectedGenre(null)} > {selectedClassification ? "CLEAR" : "Classification"} </option>
+            {classifications.map((c) => (
+              <option key={c} value={c}>
+                {c}
+              </option>
+            ))}
+          </select>
+
+          <select>
+            <option value="todos">Lang </option>
+            <option value="sub">Sub</option>
+            <option value="dub">Dub</option>
+          </select>
+
+
+          <select onChange={(e) => FiltradosContenedor(e, "Calification")}>
+            <option value="Calification">Calification</option>
+            <option value="More Popular">More Popular</option>
+            <option value="Less Popular">Less Popular</option>
+          </select>
+        </section>
+
+        {/* <section className="movies-filter">
           <select onChange={handleSelect} value={selectedGenre}>
-            <option value="" style={{ color: "red" }}>
-              {selectedGenre ? "All" : "Select a Genre"}
+            <option value="" style={{ color: "red" }} onClick={() => setSelectedGenre(null)}>
+              {selectedGenre ? "CLEAR" : "Select a Genre"}
             </option>
             {genresCurrent.map((genre) => (
               <option key={genre} value={genre} disabled={selectedGenre}>
@@ -192,7 +325,7 @@ const Movies = () => {
           </select>
 
           <select onChange={handleClassificationFilter}>
-            <option value="todos">Clasification</option>
+            <option value="todos" onClick={() => setSelectedGenre(null)} > {selectedClassification ? "CLEAR" : "Classification"} </option>
             {classifications.map((c) => (
               <option key={c} value={c}>
                 {c}
@@ -207,12 +340,12 @@ const Movies = () => {
           </select>
          
 
-          <select onClick={handleCalificationSort}>
+          <select onChange={handleCalificationSort}>
             <option value="Calification">Calification</option>
             <option value="More Popular">More Popular</option>
             <option value="Less Popular">Less Popular</option>
           </select>
-        </section>
+        </section> */}
 
         <section className="movies-Result">
           <div className="Filter-results">
@@ -220,7 +353,7 @@ const Movies = () => {
               <Movie movie={movie} />
             ))}
 
-          
+
           </div>
         </section>
 
