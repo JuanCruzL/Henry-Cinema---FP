dc3730a5fab138ea0e0c7412e8b67a1c593823d5
ZTPspartan <alfonsos99710@gmail.com>
2023-02-14 17:58:54 -0500

    css/screenings

--- Front-End/Henry-Cinema/src/Components/Dashboard Admin/screenings/NewScreening.jsx
+++ Front-End/Henry-Cinema/src/Components/Dashboard Admin/screenings/NewScreening.jsx
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from 'react';
-import {useSelector, useDispatch } from 'react-redux'
-import { getasientos} from "../../../redux/actions"
+import { useSelector, useDispatch } from 'react-redux'
+import { getasientos } from "../../../redux/actions"
+import NavBarDash from '../NavbarDash/NavBarDash';
+import SideBarDash from '../SideBarDash/SideBarDash';
+import "./newscreenings.scss";
 
 
 const RoomInputs = () => {
@@ -13,13 +16,13 @@ const RoomInputs = () => {
   const [endTime, setEndTime] = useState('');
   const [definition, setDefinition] = useState('IMAX');
   const [language, setLanguage] = useState('Sub');
-  const [seats, setSeats]= useState([])
+  const [seats, setSeats] = useState([])
   const [reservation, setReservation] = useState({});
-  
+
   useEffect(() => {
     dispatch(getasientos());
     setSeats(asientos)
-    
+
   }, [reservation]);
 
   const handleSave = () => {
@@ -34,7 +37,7 @@ const RoomInputs = () => {
     });
   };
 
-console.log(reservation)
+  console.log(reservation)
 
   const getNext5Days = () => {
     const today = new Date();
@@ -51,74 +54,96 @@ console.log(reservation)
 
   const next5Days = getNext5Days();
 
-  
+
 
   return (
-    <div>
-      <label >Room</label>
-      <select value={roomLetter} onChange={(e) => setRoomLetter(e.target.value)}>
-        {roomLetters.map((letter) => (
-          <option key={letter} value={letter}>
-            {letter}
-          </option>
-        ))}
-      </select>
-      <br />
-      <label>
-        Date:
-        <select value={date} onChange={(e) => setDate(e.target.value)}>
-          {[...Array(5)].map((_, i) => {
-            const nextDay = new Date();
-            nextDay.setDate(nextDay.getDate() + i);
-            const dateString = nextDay.toISOString().split("T")[0];
-            return (
-              <option key={dateString} value={dateString}>
-                {dateString}
-              </option>
-            );
-          })}
-        </select>
-      </label>
-      <br />
-      <label>
-        Start Time:
-        <br />
-        <input
-          type="time"
-          value={startTime}
-          onChange={(e) => setStartTime(e.target.value)}
-        />
-      </label>
-      <br />
-      <label>
-      End Time:
-      <br />
-      <input type="time" value={endTime} onChange={(e) => setEndTime(e.target.value)} />
-      </label>
-      <br />
-      <label>
-      Definition:
-      <br />
-      <select value={definition} onChange={(e) => setDefinition(e.target.value)}>
-      <option value="IMAX">IMAX</option>
-      <option value="3D">3D</option>
-      <option value="2D">2D</option>
-      </select>
-      </label>
-      <br />
-      <label>
-      Language:
-      <br />
-      <select value={language} onChange={(e) => setLanguage(e.target.value)}>
-      <option value="Sub">Sub</option>
-      <option value="Dub">Dub</option>
-      <option value="Origin">Origin</option>
-      </select>
-      </label>
-      <br />
-      <button onClick={handleSave}>Guardar</button>
-</div>
-);
+    <div className='NewScreen'>
+      <SideBarDash />
+      <div className='ContainerNewScreen'>
+        <NavBarDash />
+        <div className='InfoNewScreen'>
+          <div className="top">
+            <h1>Add New Screenings</h1>
+          </div>
+          <div className="bottom">
+            <div className="left">
+              <img
+                className="imageNF"
+                src="https://previews.123rf.com/images/foontntd/foontntd1705/foontntd170500070/77824901-menu-food-drawing-graphic-design-illustrate-objects-template.jpg"
+                alt=""
+              />
+            </div>
+            <div className="right">
+
+              <label >Room</label>
+              <select value={roomLetter} onChange={(e) => setRoomLetter(e.target.value)}>
+                {roomLetters.map((letter) => (
+                  <option key={letter} value={letter}>
+                    {letter}
+                  </option>
+                ))}
+              </select>
+               
+              <label>
+                Date:
+                <select value={date} onChange={(e) => setDate(e.target.value)}>
+                  {[...Array(5)].map((_, i) => {
+                    const nextDay = new Date();
+                    nextDay.setDate(nextDay.getDate() + i);
+                    const dateString = nextDay.toISOString().split("T")[0];
+                    return (
+                      <option key={dateString} value={dateString}>
+                        {dateString}
+                      </option>
+                    );
+                  })}
+                </select>
+              </label>
+               
+              <label>
+                Start Time:
+                 
+                <input
+                  type="time"
+                  value={startTime}
+                  onChange={(e) => setStartTime(e.target.value)}
+                />
+              </label>
+               
+              <label>
+                End Time:
+                 
+                <input type="time" value={endTime} onChange={(e) => setEndTime(e.target.value)} />
+              </label>
+               
+              <label>
+                Definition:
+                 
+                <select value={definition} onChange={(e) => setDefinition(e.target.value)}>
+                  <option value="IMAX">IMAX</option>
+                  <option value="3D">3D</option>
+                  <option value="2D">2D</option>
+                </select>
+              </label>
+               
+              <label>
+                Language:
+                 
+                <select value={language} onChange={(e) => setLanguage(e.target.value)}>
+                  <option value="Sub">Sub</option>
+                  <option value="Dub">Dub</option>
+                  <option value="Origin">Origin</option>
+                </select>
+              </label>
+               
+              <button onClick={handleSave}>Guardar</button>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default RoomInputs;
