66b67662e776c57f3f6663d4d9f1c2b2b3f83ede
emagarc <wemagw@gmail.com>
2023-02-17 22:26:02 -0300

    Dashboard update

--- Front-End/Henry-Cinema/src/Components/Dashboard Admin/foodanddrinks/Combos.jsx
+++ Front-End/Henry-Cinema/src/Components/Dashboard Admin/foodanddrinks/Combos.jsx
@@ -3,8 +3,17 @@ import "./combos.scss";
 import { SideBarDash } from "../SideBarDash/SideBarDash";
 import { NavBarDash } from "../NavbarDash/NavBarDash";
 import { CombosTable } from "./CombosTable.jsx";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 export const CombosDash = () => {
+  const loggedUser = useSelector((state) => state.currentUser);
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (!loggedUser.isAdministrator || loggedUser.isAdministrator === false) {
+      navigate("/");
+    }
+  });
   return (
     <div className="list">
       <SideBarDash />
