6b068111cf7cd921bd3a97d0970dac8d8e77fcb5
Samuel <samuel.esba@hotmail.com>
2023-02-21 00:38:02 -0400

    fix/cloudinary&mailer with local envs designed for security and mail for registration with gmail sender

--- api/src/utils/sendEmail.js
+++ api/src/utils/sendEmail.js
@@ -1,12 +1,22 @@
 const emailer = require("../services/emailer.js");
 
 function sendEmail(user) {
-    const userData = {
+
+    if(user.notifications === true) {
+      const userData = {
+          email: user.email,
+          userName: user.userName,
+          type: "notifications",
+        };
+      emailer.sendMail(userData);
+    } else {
+      const userData = {
         email: user.email,
         userName: user.userName,
-        type: "notifications",
-      };
-    emailer.sendMail(userData);
+        type: "registration",
+      }
+      emailer.sendMail(userData);
+    }
 }
 
 // sendEmail({email: "gonzales@hotmail.com", userName: "juanito"});
