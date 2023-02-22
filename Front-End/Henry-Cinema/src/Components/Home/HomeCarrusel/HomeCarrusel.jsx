f5b7234d6a9851fdf519727614a48461b44c6050
ZTPspartan <alfonsos99710@gmail.com>
2023-02-22 12:21:21 -0500

    Solucion al carrusel HOME

--- Front-End/Henry-Cinema/src/Components/Home/HomeCarrusel/HomeCarrusel.jsx
+++ Front-End/Henry-Cinema/src/Components/Home/HomeCarrusel/HomeCarrusel.jsx
@@ -3,10 +3,10 @@ import { Link } from "react-router-dom";
 import "../HomeCarrusel/HomeCarrusel.css";
 
 export default function HomeCarrusel({ cartelera }) {
-  var tiempo;
+  let tiempo;
   let actual = 0;
   var cantidad = 0;
-  const velocidad = 5000;
+  const velocidad = 2000;
   mostrar(actual);
   tiempo = setInterval(() => {
     Next();
