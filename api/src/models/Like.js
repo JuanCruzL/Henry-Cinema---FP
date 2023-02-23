24f0009fe58d7696e5a5e7a453537dcdb589fd62
unknown <juanjc2002@hotmail.com>
2023-02-23 00:07:12 -0300

    working on likes

--- api/src/models/Like.js
+++ api/src/models/Like.js
@@ -1,30 +1,49 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 
 module.exports = (sequelize) => {
-  sequelize.define('Like', {
-    id: {
-      type: DataTypes.UUID,
-      defaultValue: DataTypes.UUIDV4,
-      primaryKey: true,
-    },
-    userId: {
-      type: DataTypes.UUID,
-      allowNull: false,
-      references: {
-        model: 'User',
-        key: 'id'
-      }
-    },
-    movieId: {
+  sequelize.define(
+    "Like",
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+      },
+      userId: {
         type: DataTypes.UUID,
         allowNull: false,
         references: {
-          model: 'Movie',
-          key: 'id'
-        }
-    }
-  },
-  {
-    timestamps: false
-  });
+          model: "Users",
+          key: "id",
+        },
+      },
+      movieId: {
+        type: DataTypes.UUID,
+        allowNull: false,
+        references: {
+          model: "Movies",
+          key: "id",
+        },
+      },
+      User_Likes: {
+        type: DataTypes.UUID,
+        references: {
+          model: "Users",
+          key: "id",
+        },
+        onDelete: "SET NULL",
+        onUpdate: "CASCADE",
+      },
+      Movie_Likes: {
+        type: DataTypes.UUID,
+        references: {
+          model: "Movies",
+          key: "id",
+        },
+        onDelete: "SET NULL",
+        onUpdate: "CASCADE",
+      },
+    },
+    { timestamps: false }
+  );
 };
